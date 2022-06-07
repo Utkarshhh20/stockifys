41ac2e200ae860bd503b786d3134bed476e9409a
Utkarsh Gupta <88526325+Utkarshhh20@users.noreply.github.com>
2022-06-07 22:52:35 +0400

    Update stockifyssite.py

--- stockifyssite.py
+++ stockifyssite.py
@@ -1,3 +1,46 @@
 import psycopg2, psycopg2.extras
 connection = psycopg2.connect(host=config.DB_HOST, database=config.DB_NAME, user=config.DB_USER, password=config.DB_PASS)
 cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+pattern = st.sidebar.selectbox(
+        "Which Pattern?",
+        ("engulfing", "threebar")
+    )
+if pattern == 'engulfing':
+        cursor.execute("""
+            SELECT * 
+            FROM ( 
+                SELECT day, open, close, stock_id, symbol, 
+                LAG(close, 1) OVER ( PARTITION BY stock_id ORDER BY day ) previous_close, 
+                LAG(open, 1) OVER ( PARTITION BY stock_id ORDER BY day ) previous_open 
+                FROM daily_bars
+                JOIN stock ON stock.id = daily_bars.stock_id
+            ) a 
+            WHERE previous_close < previous_open AND close > previous_open AND open < previous_close
+            AND day = '2021-02-18'
+        """)
+if pattern == 'threebar':
+        cursor.execute("""
+            SELECT * 
+            FROM ( 
+                SELECT day, close, volume, stock_id, symbol, 
+                LAG(close, 1) OVER ( PARTITION BY stock_id ORDER BY day ) previous_close, 
+                LAG(volume, 1) OVER ( PARTITION BY stock_id ORDER BY day ) previous_volume, 
+                LAG(close, 2) OVER ( PARTITION BY stock_id ORDER BY day ) previous_previous_close, 
+                LAG(volume, 2) OVER ( PARTITION BY stock_id ORDER BY day ) previous_previous_volume, 
+                LAG(close, 3) OVER ( PARTITION BY stock_id ORDER BY day ) previous_previous_previous_close, 
+                LAG(volume, 3) OVER ( PARTITION BY stock_id ORDER BY day ) previous_previous_previous_volume 
+            FROM daily_bars 
+            JOIN stock ON stock.id = daily_bars.stock_id) a 
+            WHERE close > previous_previous_previous_close 
+                AND previous_close < previous_previous_close 
+                AND previous_close < previous_previous_previous_close 
+                AND volume > previous_volume 
+                AND previous_volume < previous_previous_volume 
+                AND previous_previous_volume < previous_previous_previous_volume 
+                AND day = '2021-02-19'
+        """)
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        st.image(f"https://finviz.com/chart.ashx?t={row['symbol']}")
