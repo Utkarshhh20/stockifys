06edd88aa790cfa3130991d67b7866f34136c49c
Utkarsh Gupta <88526325+Utkarshhh20@users.noreply.github.com>
2022-06-07 22:51:33 +0400

    Update stockifyssite.py

--- stockifyssite.py
+++ stockifyssite.py
@@ -1,2 +1,3 @@
 import psycopg2, psycopg2.extras
-import psycopg
+connection = psycopg2.connect(host=config.DB_HOST, database=config.DB_NAME, user=config.DB_USER, password=config.DB_PASS)
+cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
